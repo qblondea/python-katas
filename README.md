761855acc94f102c3549bf9c0ae25973153e0e32
qblondea <quentin.blondeau22@gmail.com>
2025-08-27 23:34:02 +0200

    Update

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 #### Katas 2 - Factorial F(n)
 #### Katas 3 - Fibonacci F(n)
 #### Katas 4 - is Anagram ? 
-#### Katas 5 
+#### Katas 5 - WIP
--- tests/tests.py
+++
@@ -1,4 +0,0 @@
-from katas.palindrome import is_palindrome_1
-from katas.factorial import factorial
-from katas.fibonacci import fib
-from katas.anagram import is_anagram
